a273dd1b05abc91c089b33b6a1cd7b32a899e876
tacsfaiteau <55443318+tacsfaiteau@users.noreply.github.com>
2019-09-24 08:43:17 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,29 @@
+// Name: Omar Faiteau
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-09-24 Classwork
+
+#include <iostream>
+
+int main()
+{
+	int x = 10000;
+	int y = 5;
+	float b = 6;
+	double g = 15;
+	bool d = 88;
+	char m = 3;
+	int z = 54;
+	float w = 4;
+	double h = 34;
+	bool  f = 261;
+	char i = 12;
+	float t = 16;
+	char c = 18;
+	int u = 28;
+	bool p = 39;
+
+
+		std::cout << sizeof(x) << std:endl
+}               
