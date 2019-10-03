af6ae07a3bd893a86f9447207942044b09c8ad70
tacsfaiteau <55443318+tacsfaiteau@users.noreply.github.com>
2019-10-03 08:39:16 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Name: Omar Faiteau
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " number?  ";
+	int x;
+	cin >> x;
+	cout << "Cool, " << x << " is my favorite number too!" << endl;
+}
